aeb49b1465baea2695dbe2e5c6b9589fd52b8fdb
sarjunwadkar <54752919+sarjunwadkar@users.noreply.github.com>
2021-09-12 11:30:13 +0100

    dynamic path added

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 print("hello world !!!!!")
 app = Flask(__name__)
 
@@ -6,6 +7,34 @@ app = Flask(__name__)
 @app.route("/hello")
 def hello_world():
   return "hello world118"
+
+@app.route("/test/<s_query>")
+def search(s_query):
+  return s_query
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+  print(value + 1)
+  return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+  print(value + 1.45, flush=True)
+  return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+  print( value, flush=True)
+  return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+  if name.lower() == "michael":
+    return "Hello, {}".format(name), 200
+  else:
+    return "Not found", 404 
+
 
 if __name__ == '__main__':
   app.run(debug=True)
