96a8a358c0eccb865f1615ad67dfef8d3fdcb6fb
sarjunwadkar <54752919+sarjunwadkar@users.noreply.github.com>
2021-09-18 17:00:55 +0100

    Register and login form added - upto page 152

--- flasktaskr/project/_config.py
+++ flasktaskr/project/_config.py
@@ -4,8 +4,6 @@ print("hello world ", file=sys.stderr)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True 
 SECRET_KEY = ')g%\xea\x88*\x8b\xf8\x8e@(O5\x05g_\xe558\xa64\xf0\x83('
 
--- flasktaskr/project/forms.py
+++ flasktaskr/project/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField, DateField, IntegerField, SelectField 
+from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField 
 
-from wtforms.validators import DataRequired 
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class AddTaskForm(FlaskForm):
   task_id = IntegerField()
@@ -21,3 +21,15 @@ class AddTaskForm(FlaskForm):
 
   status = IntegerField('Status')
   
+
+class RegisterForm(FlaskForm):
+  name = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+  email = StringField('Email', validators=[DataRequired(), Length(min=4, max=40)])
+  password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=40)])
+  confirm = PasswordField('Repeat Password', validators=[DataRequired(), 
+                          EqualTo('password', message='Passwords must match')])
+
+class LoginForm(FlaskForm):
+  name = StringField('Username', validators=[DataRequired()])
+  password = PasswordField('Password', validators=[DataRequired()])
+
